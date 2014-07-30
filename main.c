b4480acadfab6895b0f3ef7bdac629b96d69c295
Neo Chen <netkiller@msn.com>
2014-07-30 09:50:23 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GT1 MySQL Plugin
+//
+//  Created by Neo Chen on 7/30/14.
+//  Copyright (c) 2014 Neo Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
